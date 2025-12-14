fdf6d6751e5d615deb71ca1594cfcece1921f342
APPLE <apple@Roselyn-MacBook-Air.local>
2025-12-14 19:12:15 +0530

    fix whatapp broken link

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -1,14 +1,16 @@
-import "../styles/whatsapp.css"
+import "../styles/whatsapp.css";
 
-export default function WhatsApp(){
-    return (
-        <>
-        
-        <div className="whatsapp_wrapper">
-            <a href="https://wa.me/+918652339403&text=I'm%20interested%20in%20your%20services" target="_blank" title="whatsapp chat">
-                <img height={50} src="./whatsapp.png" alt="Contact Me" />
-            </a>
-        </div>
-        </>
-    )
+export default function WhatsApp() {
+  return (
+    <div className="whatsapp_wrapper">
+      <a
+        href="https://wa.me/918652339403?text=I'm%20interested%20in%20your%20services"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="WhatsApp chat"
+      >
+        <img height={50} src="/whatsapp.png" alt="Contact Me" />
+      </a>
+    </div>
+  );
 }
